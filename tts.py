116a57c26be1e38d57ab023a7a14a3395e2f1c7f
schreckman <manuel.schreck.1999@web.de>
2023-04-04 20:24:45 +0200

    added error message and coloring

--- tts.py
+++ tts.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-
 from gtts import gTTS
 import playsound
 import os
 import sys
 from colorama import Fore
-
-# TODO python3 -m pip install -U nuitka
-# TODO sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-4.0
-# TODO sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0
-# TODO pip3 install pycairo
-# TODO pip3 install PyGObject
 
 if len(sys.argv) < 2:
     print(Fore.RED, "Please use the program like this:")
